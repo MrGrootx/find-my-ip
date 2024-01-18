74dc2168fef690e9fbafbf4fd9a2073a9201c02c
Mr Groot <113226362+MrGrootx@users.noreply.github.com>
2024-01-18 05:58:58 +0530

    fixed: btn bug

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,24 @@
-const api = "https://api.ipify.org?format=json";
+document.addEventListener("DOMContentLoaded", loadAllcontent);
 
-fetch(api)
-  .then((data) => data.json())
-  .then((data) => {
-    const footertxt = document.querySelector("#footertxt");
-    console.log(data.ip);
-    footertxt.textContent = data.ip;
-  })
-  .catch((err) => {
-    console.log(err);
-    footertxt.textContent = "there is some errors while fetching datas";
+function loadAllcontent() {
+  const api = "https://api.ipify.org?format=json";
+
+  fetch(api)
+    .then((data) => data.json())
+    .then((data) => {
+      const footertxt = document.querySelector("#footertxt");
+      console.log(data.ip);
+      footertxt.textContent = data.ip;
+    })
+    .catch((err) => {
+      console.log(err);
+      footertxt.textContent = "there is some errors while fetching datas";
+    });
+
+  const agreeBtn = document.querySelector("#agreeBtn");
+  const agreesection = document.querySelector("#agreesection");
+
+  agreeBtn.addEventListener("click", () => {
+    agreesection.classList.add("hidden");
   });
-
-const agreeBtn = document.querySelector("#agreeBtn");
-const agreesection = document.querySelector("#agreesection");
-
-
-agreeBtn.addEventListener("click", () => {
-  agreesection.classList.add("hidden");
-})
+}
